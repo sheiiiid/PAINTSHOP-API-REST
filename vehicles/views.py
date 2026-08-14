905f6c0fe8d974892001d29302df5842ceb84589
sheiiiid <sheiiid.dev@gmail.com>
2026-08-14 14:51:23 -0400

    feat(vehicle): create vehicle model, serializers, views

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vehicle
+from .serializers import VehicleSerializer
 
 # Create your views here.
+class VehicleView(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
