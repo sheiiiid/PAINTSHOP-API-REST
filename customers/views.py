8a60cb6e8b6ad13c6e964f47114e4b8f6cfd1d45
sheiiiid <sheiiid.dev@gmail.com>
2026-08-14 04:34:41 -0400

    feat(customers): implement customer serializer, urls, views, and initial migrations

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer
+from .serializers import CustomerSerializer
 
 # Create your views here.
+class CustomerView(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
