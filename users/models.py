da63a96eecda81fe1c95872a053d5d18c21bd438
sheiiiid <sheiiid.dev@gmail.com>
2026-08-14 23:36:31 -0400

    fix(user): fix user model, update db

--- users/models.py
+++ users/models.py
@@ -83,4 +83,4 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = "Users"
 
     def __str__(self):
-        return self.email
+        return self.name + " " + self.paternal_last_name + " " + self.maternal_last_name
